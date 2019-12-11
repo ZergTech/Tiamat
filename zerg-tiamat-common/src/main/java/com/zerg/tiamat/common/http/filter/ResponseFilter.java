c027c795327a7b7aa53fb75011855682e5fb64de
zerg <xuyang17@guazi.com>
2019-12-12 01:10:51 +0800

    init

--- zerg-tiamat-common/src/main/java/com/zerg/tiamat/common/http/filter/ResponseFilter.java
+++
@@ -1,9 +0,0 @@
-package com.zerg.tiamat.common.http.filter;
-
-/**
- * @author : xuyang
- * @date : 2019-09-05 21:43
- */
-public interface ResponseFilter {
-    String filter(String s);
-}
--- zerg-tiamat-service/src/main/java/com/zerg/tiamat/service/local/HealthCheckService.java
+++
@@ -1,9 +0,0 @@
-package com.zerg.tiamat.service.local;
-
-/**
- * @author : xuyang
- * @date : 2019-11-22 17:30
- */
-public interface HealthCheckService {
-    String healthCheck();
-}
